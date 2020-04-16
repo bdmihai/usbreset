fe405120e48eee72d3f413444409ea95383a3fae
MihaiB <bdmihai@gmail.com>
2020-04-16 21:54:13 +0200

    Initial revision

---
+++ usbreset.c
@@ -0,0 +1,41 @@
+/* https://askubuntu.com/questions/645/how-do-you-reset-a-usb-device-from-the-command-line */
+/* usbreset -- send a USB port reset to a USB device */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+
+#include <linux/usbdevice_fs.h>
+
+
+int main(int argc, char **argv)
+{
+    const char *filename;
+    int fd;
+    int rc;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: usbreset device-filename\nExample:\n	lsusb\n	Bus 002 Device 003: ID 0fe9:9010 DVICO\n	sudo ./usbreset /dev/bus/usb/002/003\n");
+        return 1;
+    }
+    filename = argv[1];
+
+    fd = open(filename, O_WRONLY);
+    if (fd < 0) {
+        perror("Error opening output file");
+        return 1;
+    }
+
+    printf("Resetting USB device %s\n", filename);
+    rc = ioctl(fd, USBDEVFS_RESET, 0);
+    if (rc < 0) {
+        perror("Error in ioctl");
+        return 1;
+    }
+    printf("Reset successful\n");
+
+    close(fd);
+    return 0;
+} 
